69a6f598443014c0201ca197a1fd159ab5f6c90c
Jausefine <81740934+Jausefine@users.noreply.github.com>
2021-04-18 18:54:01 +0900

    -a[2] and changes default parameters

--- second.py
+++ second.py
@@ -7,9 +7,9 @@ import numpy as np
 # Right-hand-side function of the ODE
 def dynamics(y, t, a):
     """second-order linear ODE:
-        d2y/dt2 = a[0]*dy/dt + a[1]*y + a[2]"""
+        d2y/dt2 = a[0]*dy/dt + a[1]*y - a[2]"""
     yt, dydt = y
-    return(np.array([dydt, a[0]*dydt + a[1]*yt + a[2]]))
+    return(np.array([dydt, a[0]*dydt + a[1]*yt - a[2]]))
 
 # Name of the system
 name = 'second'
@@ -18,7 +18,7 @@ name = 'second'
 initial_state = [1, 0]
 
 # Default parameters
-parameters = ([-0.1, -1, 0],)
+parameters = ([-0.2, -2, 0],)
 
 # For adding to the system list
 name_state_parameters = [name, initial_state, parameters]
